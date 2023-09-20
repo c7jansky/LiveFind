cfea6d7471f600ad60d93968e2ee14bde16f9b8f
Chris Jansky <c7jansky@yahoo.com>
2023-09-19 21:24:06 -0700

    Initial Commit

---
+++ LiveFind/LiveFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFindApp.swift
+//  LiveFind
+//
+//  Created by keckuser on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LiveFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
