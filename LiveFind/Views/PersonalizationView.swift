23e1b72f9629032601a0e7f9a8642f64ea924d30
berkematthew <73667078+berkematthew@users.noreply.github.com>
2023-10-31 13:02:02 -0700

    current

--- LiveFind/Views/PersonalizationView.swift
+++ LiveFind/Views/PersonalizationView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct PersonalizationView: View {
+    let Primary = Color("PrimaryColor")
+    let Secondary = Color("SecondaryColor")
     var body: some View {
         ZStack{
             Color("PrimaryColor")
@@ -22,5 +24,21 @@ struct PersonalizationView: View {
 struct PersonalizationView_Previews: PreviewProvider {
     static var previews: some View {
         PersonalizationView()
+    }
+}
+
+
+extension PersonalizationView{
+    
+    private var titleSection: some View{
+        VStack(alignment: .leading, spacing: 8){
+            Text("Dr. Dre")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Secondary)
+            Text("Rap, R&B")
+                .font(.subheadline)
+                .foregroundColor(Secondary)
+        }
     }
 }
