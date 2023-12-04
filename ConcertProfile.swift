3b5629f60afec2484bc2b057e43d0aab31874b2f
berkematthew <73667078+berkematthew@users.noreply.github.com>
2023-12-03 17:16:14 -0800

    profile for concerts

---
+++ ConcertProfile.swift
@@ -0,0 +1,20 @@
+//
+//  ConcertProfile.swift
+//  LiveFind
+//
+//  Created by keckuser on 12/3/23.
+//
+
+import SwiftUI
+
+struct ConcertProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConcertProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        ConcertProfile()
+    }
+}
